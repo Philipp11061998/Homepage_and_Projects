adce92b6ea0570938a700bf5518f2e28f56af581
Philipp11061998 <philippkraut25@gmail.com>
2024-08-10 15:55:02 +0200

    einfach nur eine leere Zeile xD

--- ToDoApp/src/main.js
+++ ToDoApp/src/main.js
@@ -14,6 +14,7 @@ document.addEventListener("DOMContentLoaded", () => {
     loginApp.mount("#login");
 
     const username = document.getElementById("username");
+    
     if (username) {
         username.focus();
     }
